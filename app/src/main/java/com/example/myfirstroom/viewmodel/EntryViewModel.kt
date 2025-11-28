26adf6ca6e4085de21f1f6a2a5acc61a76f27e7c
MariskaEsaPurnomo <mariskaesapurnomo@gmail.com>
2025-11-28 14:17:28 +0700

    Menambahkan logika uiStateSiswa dan fungsi validasiInput untuk memastikan data siswa terisi dengan benar

---
+++ app/src/main/java/com/example/myfirstroom/viewmodel/EntryViewModel.kt
@@ -0,0 +1,24 @@
+package com.example.myfirstroom.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.myfirstroom.repositori.RepositoriSiswa
+
+class EntryViewModel(private val repositoriSiswa: RepositoriSiswa) : ViewModel(){
+    /**
+     * Berisi status Siswa saat ini
+     */
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    /**
+     * Fungsi untuk memvalidasi input
+     */
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa): Boolean {
+        return with(uiState) {
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+
+}
